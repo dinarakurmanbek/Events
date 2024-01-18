57cad8ca244678b6ecae0a36781428aaa2257e1f
dinarakurmanbek <86626782+dinarakurmanbek@users.noreply.github.com>
2024-01-18 16:22:24 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ window.addEventListener('appinstalled', (event) => {
 
 // Service Worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('js/sw.js')
     .then((registration) => {
       console.log('Service Worker registriert:', registration);
     })
